8b835da0a7baff5279d935909281e54a077fa155
tongyuebin <tongyuebin@ihammertech.com>
2021-07-19 15:16:06 +0800

    完成了报表管理的开发

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ import Params from './components/goods/Params.vue'
 import GoodsList from './components/goods/List.vue'
 import GoodsAdd from './components/goods/Add.vue'
 import Order from './components/order/Order.vue'
+import Report from './components/report/Report.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -28,7 +29,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: GoodsList },
       { path: '/goods/add', component: GoodsAdd },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
     ]
   },
   { path: '/login', component: Login }
